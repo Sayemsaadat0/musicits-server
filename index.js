0f532afcbc6220439f3b608a5d69c7d580981b9e
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-06-08 17:46:18 +0600

    second commit

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ app.use(express.json())
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.njebycd.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.njebycd.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,9 +24,13 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
      client.connect();
 
+    //  collections
+     const classCollection = client.db("musicits").collection("classes");
+     const instractorCollection = client.db("musicits").collection("instractor");
+     const popularclassCollection = client.db("musicits").collection("popularclass");
+     const popularinstractorCollection = client.db("musicits").collection("popularinstractor");
 
 
 
@@ -37,7 +39,38 @@ async function run() {
 
 
 
-    // Send a ping to confirm a successful connection
+
+
+
+
+
+
+
+    //  getting data from the databse
+     app.get('/classes', async(req,res)=>{
+        const result = await classCollection.find().toArray()
+        res.send(result)
+     })
+     app.get('/instractor', async(req,res)=>{
+        const result = await instractorCollection.find().toArray()
+        res.send(result)
+     })
+     app.get('/popularclass', async(req,res)=>{
+        const result = await popularclassCollection.find().toArray()
+        res.send(result)
+     })
+     app.get('/popularinstractor', async(req,res)=>{
+        const result = await popularinstractorCollection.find().toArray()
+        res.send(result)
+     })
+
+
+
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   }
